fecd38d260fe689f8cac62af8324f084e03cfbdc
eyalmagnus <eyalmagnus@gmail.com>
2020-09-23 12:05:59 +0300

    Portal Search Pager ready

--- src/components/PortalSearchPager/PortalSearchPager.js
+++ src/components/PortalSearchPager/PortalSearchPager.js
@@ -4,38 +4,39 @@ import arrow from "../../assets/images/arrow_down.svg";
 import arrowdim from "../../assets/images/arrow_dim.svg";
 
 const PortalSearchPager = (props) => {
-  const [page, setPage] = useState(props.onPage);
-  var pageC;
+
+  const { onPage, onPageChange, pages, pHolder } = props;
 
   function handleClick(dir) {
-    pageC = page;
-    pageC = pageC + dir;
-    if ((pageC > props.pages || pageC < 1)) {
-      pageC = pageC - dir;
-    };
-    setPage(pageC);
-    // props.onPageChange(pageC);
-    console.log(page);
+    let newPage = onPage + dir;
+    if ((newPage > pages || newPage < 1)) {
+      newPage = newPage - dir;
+    } else { onPageChange(newPage); };
+    // 
+    console.log(onPage);
   }
 
-  var backIsOn = (page == 1) ? false : true;
-  var fwdIsOn = (page == props.pages) ? false : true;
-  var numPos = (page > 9) ? "20px" : "25px";
+  const backIsOn = (onPage == 1) ? false : true;
+  const fwdIsOn = (onPage == pages) ? false : true;
+  const numPos = (onPage > 9) ? "20px" : "25px";
+  let showPager = null;
+  if (pages > 1) {
+    showPager = <div className="pager">
+      <img src={backIsOn ? arrow : arrowdim} className="buttons" onClick={() => handleClick(-1)}></img>
+      <span style={{ right: numPos }} id="pagerNum">{onPage}</span>
+      <img src={fwdIsOn ? arrow : arrowdim} className="buttons" onClick={() => handleClick(1)}></img>
+    </div>
+  }
 
   return (
     <div className="pspBox">
       <div className="search">
-        <input placeholder={props.pHolder}></input>
+        <input placeholder={pHolder}></input>
       </div>
-      <div className="pager">
-
-        <img src={backIsOn ? arrow : arrowdim} className="buttons" onClick={() => handleClick(-1)}></img>
-        <span style={{ right: numPos }} id="pagerNum">{page}</span>
-        <img src={fwdIsOn ? arrow : arrowdim} className="buttons" onClick={() => handleClick(1)}></img>
-      </div>
+      {showPager}
     </div>
   );
 };
-{/* <PortalSearchPager onPage={Current page on render} pages={The total number of pages} pHolder={"חיפוש משתמשים"} onPageChange={a Function In Parent}/> put into parent/ } */ }
+{/* <PortalSearchPager onPage={Current page on render} pages={The total number of pages} pHolder={"חיפוש משתמשים"} ononPagehange={a Function In Parent}/> put into parent/ } */ }
 
 export default PortalSearchPager;
--- src/pages/users/UsersPage.js
+++ src/pages/users/UsersPage.js
@@ -1,21 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './users.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
+import PortalSearchPager from '../../components/PortalSearchPager/PortalSearchPager';
 
 const UsersPage = (props) => {
     const { handleLogout } = props;
     const activeUser = useContext(ActiveUserContext);
-
+    const [onPage, setOnPage] = useState(1)
     if (!activeUser) {
         return <Redirect to='/' />
+    }
+    const handlePageClick = (newPage) => {
+        setOnPage(newPage)
     }
 
     return (
         <div className="p-users">
-            <PortalNavbar handleLogout={handleLogout} />
+            {/* <PortalNavbar handleLogout={handleLogout} /> */}
             <h1>משתמשים</h1>
+            <PortalSearchPager onPage={onPage} pages={25} pHolder={"חיפוש משתמשים"} onPageChange={handlePageClick} />
         </div>
     );
 }
