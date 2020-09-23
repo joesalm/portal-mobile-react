a9b9d1278b7292faac3ca5824e49bdaa895099cb
eyalmagnus <eyalmagnus@gmail.com>
2020-09-23 22:32:21 +0300

    Pretyer and cleaner

--- src/components/PortalSearchPager/PortalSearchPager.js
+++ src/components/PortalSearchPager/PortalSearchPager.js
@@ -2,50 +2,58 @@ import React, { useState } from "react";
 import "./PortalSearchPager.css";
 import arrow from "../../assets/images/arrow_down.svg";
 import arrowdim from "../../assets/images/arrow_dim.svg";
+
 /*This is the Portal Search Pager stateless component.
 Props are:
-  onPage - number = The current page on render
-  onPageChange - function => The function to call when page number is changed. Parent recieves the new page number in the call.
-  pages - number = How many total pages. Pager only renders if >1
-  pHolder - string = Place Holder 
-  onSearchSubmit  - function => The function to call when a value is entered in the search.
-Parent must manage page State
+      onPage - number = The current page to render
+      onPageChange - function => To call when page number is changed. Parent recieves the new page number in the call.
+      pages - number = The total number of pages. Pager only renders if it is larger than >1
+      pHolder - string = Place Holder 
+      onSearchSubmit  - function => To call when a value is entered in the search.
+Parent must manage the current page State
+Rendring example:
+    <PortalSearchPager onPage={onPage} pages={pages} pHolder={"חיפוש משתמשים"} onPageChange={handlePageChange} onSearchSubmit={handelSearchSubmit} />
+ */
 
-*/
 const PortalSearchPager = (props) => {
-
   const { onPage, onPageChange, pages, pHolder, onSearchSubmit } = props;
 
   const handleClick = (dir) => {
     let newPage = onPage + dir;
     if (!(newPage > pages || newPage < 1)) {
       onPageChange(newPage);
-    };
-    console.log("in function: " + onPage);
-  }
-  console.log("out in App: " + onPage);
+    }
+  };
 
-  const backIsOn = (onPage == 1) ? false : true;
-  const fwdIsOn = (onPage == pages) ? false : true;
+  const backSrc = onPage == 1 ? arrowdim : arrow;
+  const fwdSrc = onPage == pages ? arrowdim : arrow;
   let showPager = null;
   if (pages > 1) {
-    showPager = <div className="pager">
-      <img src={backIsOn ? arrow : arrowdim} className="buttons" onClick={() => handleClick(-1)}></img>
-      <span id="pagerNum">{onPage}</span>
-      <img src={fwdIsOn ? arrow : arrowdim} className="buttons" onClick={() => handleClick(1)}></img>
-    </div>
+    showPager = (
+      <div className="pager">
+        <img
+          src={backSrc}
+          className="buttons"
+          onClick={() => handleClick(-1)}
+        ></img>
+        <span id="pagerNum">{onPage}</span>
+        <img
+          src={fwdSrc}
+          className="buttons"
+          onClick={() => handleClick(1)}
+        ></img>
+      </div>
+    );
   }
-
-
 
   const input = React.createRef();
 
-
   function handleSubmit() {
-    if (input.current.value !== "") { onSearchSubmit(input.current.value); }
+    const value = input.current.value;
+    if (value !== "") {
+      onSearchSubmit(value);
+    }
   }
-
-
 
   return (
     <div className="pspBox">
@@ -58,6 +66,5 @@ const PortalSearchPager = (props) => {
     </div>
   );
 };
-{/* <PortalSearchPager onPage={Current page on render} pages={The total number of pages} pHolder={"חיפוש משתמשים"} ononPagehange={a Function In Parent}/> put into parent/ } */ }
 
 export default PortalSearchPager;
