926fdebe7b049e79449c657cd8c51a35b755c710
Tamir <kaliftamir@gmail.com>
2020-09-24 15:44:51 +0300

    first push

--- src/pages/courses/CoursesPage.js
+++ src/pages/courses/CoursesPage.js
@@ -17,7 +17,7 @@ const CoursesPage = (props) => {
     const [currentPage, setCurrentPage] = useState(1)
     const [currentSearch, setCurrentSearch] = useState("")
 
-    const [courseId, setCourseId] = useState("") // redirect to specific
+    const [courseRedirect, setCourseRedirect] = useState("") 
 
 
     const [courses, setCourses] = useState([])
@@ -32,15 +32,14 @@ const CoursesPage = (props) => {
         console.log(currentSearch)
     }
 
-    const handleCourseOnClick = (e) => {
-        console.log(e)
-        console.log(courseId)
-        setCourseId("893")
-       // alert(e)        
+    const handleCourseOnClick = (e) => {        
+        console.log(e.courseid)
+        setCourseRedirect(e.courseid)
+              
     }
     
     // hard coded headers
-    const headers = [{ key: "courseid", header: "שם קורס מקוצר" }, { key: "project", header: "שם פרוייקט" }, { key: "teachers", header: "מדריך" }];
+    const headers = [{ key: "subname", header: "שם קורס מקוצר" }, { key: "project", header: "שם פרוייקט" }, { key: "teachers", header: "מדריך" }];
     //const data = [{ id: "12212", fname: "Front-End", lname: "React", email: "ניר חנס" }, { id: "2212", fname: "Back-End", lname: "PHP", email: "ג'ון דו" }]
     const data = courses
     
@@ -49,15 +48,11 @@ const CoursesPage = (props) => {
    
     useEffect(() => {
         
-        const data = {'search': currentSearch, 'sorting': "courseid", 'desc':false, 'coursestatus': 1, 'page': currentPage};
+        const data = {search: currentSearch, sorting: "courseid", desc:false, coursestatus: 1, page: currentPage};
         server(activeUser, data, "SearchCourses").then(res => {
             if (res.data.error) {
                 alert("error in courses");
             } else {
-                
-                console.log(res.data.courses);
-                setCurrentPage(currentPage)
-                setCurrentSearch(currentSearch)
 
                 const coursesToDisplay = res.data.courses
                 setCourses(coursesToDisplay) 
@@ -74,15 +69,17 @@ const CoursesPage = (props) => {
     
     //----------------------------------------
 
-    // when clicking on spesific course
-    if (!courseId=="") {
-        return <Redirect to={`/courses/:${courseId}`} />
-        //return <Redirect to='/courses/:893' />
-    } 
-    
     if (!activeUser) {
         return <Redirect to='/' />
     }
+
+    // when clicking on spesific course
+    if (courseRedirect !== "") {
+        return <Redirect to={`/courses/:${courseRedirect}`} />
+       
+    } 
+    
+ 
 
     return (
         <div className="p-courses">
