e8a62418332630157e537f10ede621e76e181e8a
Tamir <kaliftamir@gmail.com>
2020-09-26 12:28:17 +0300

    set the layout in CoureDetailsPage

--- src/pages/courses/CourseDetailsPage.js
+++ src/pages/courses/CourseDetailsPage.js
@@ -4,6 +4,11 @@ import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
 import PortalTabView from '../../components/PortalTabView/PortalTabView';
+import SaveIcon from "../../assets/images/noun_save.svg";
+import CopyIcon from "../../assets/images/noun_copy.svg";
+import BackArrowIcon from "../../assets/images/noun_back_arrow.svg";
+import { Row,Col } from 'react-bootstrap';
+
 
 
 const CourseDetailsPage = (props) => {
@@ -25,7 +30,21 @@ const CourseDetailsPage = (props) => {
         <div className="p-course-details">
             <PortalNavbar handleLogout={handleLogout}/>
             <p>קורסים</p>
-            <h1>שם קורס מקוצר</h1>
+
+            <Row className="header">
+                <Col className="col-sm-2">
+                    <h1>שם קורס מקוצר</h1>
+                </Col>
+
+                <Col className="icons col-sm-9">
+                    <img src={BackArrowIcon}></img>
+                    <img src={CopyIcon}></img>
+                    <img src={SaveIcon}></img>
+                </Col>                
+                
+            </Row>
+            
+            
             <PortalTabView options={options} handleSelection={handleSelection}/>
 
         </div>
