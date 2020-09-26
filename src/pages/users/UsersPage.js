ec8a4542e4fe228a77e1aea28ed97697dd0b5207
eyalmagnus <eyalmagnus@gmail.com>
2020-09-26 21:38:19 +0300

    pager flips pages; pages from server is added to state

--- src/pages/users/UsersPage.js
+++ src/pages/users/UsersPage.js
@@ -12,38 +12,42 @@ import UsersButtonSetComp from "../../components/usersButtonSetComp/UsersButtonS
 const UsersPage = (props) => {
   const { handleLogout } = props;
   const activeUser = useContext(ActiveUserContext);
-  const [currentPage, setOnPage] = useState(1);
-  const [data, setData] = useState([{ id: "12212", fname: "ניר", lname: "חנס", email: "nirchannes@gmail.com" }, { id: "2212", fname: "רונית", lname: "אברהמי", email: "ronit.av@gmail.com" }]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [data, setData] = useState([{ id: "12212", firstname: "ניר", lastname: "חנס", email: "nirchannes@gmail.com" }, { id: "2212", firstname: "רונית", lastname: "אברהמי", email: "ronit.av@gmail.com" }]);
   const callData = {
     desc: false,
-    page: 0,
+    page: currentPage - 1,
     search: "",
     sorting: "userid",
     userstatus: 1,
   }
-
   useEffect(() => {
-    server(activeUser, callData, "SearchStaffUnderMe").then(res => { const resData = res.data.users; setData(resData) })
+    server(activeUser, callData, "SearchStaffUnderMe").then(res => {
+      const resData = res.data.users;
+      setData(resData)
+      // pages = res.data.pages;
+      console.log(res.data.pages);
+    })
   },
-    []);
+    [currentPage]);
 
   if (!activeUser) {
     return <Redirect to="/" />;
   }
   const headers = [{ key: "firstname", header: "שם פרטי" }, { key: "lastname", header: "שם משפחה" }, { key: "email", header: "אימייל" }];
-  // setData([{ id: "12212", fname: "ניר", lname: "חנס", email: "nirchannes@gmail.com" }, { id: "2212", fname: "רונית", lname: "אברהמי", email: "ronit.av@gmail.com" }])
+  // setData([{ id: "12212", firstname: "ניר", lastname: "חנס", email: "nirchannes@gmail.com" }, { id: "2212", firstname: "רונית", lastname: "אברהמי", email: "ronit.av@gmail.com" }])
 
 
 
 
   const handlePageChange = (newPage) => {
-    setOnPage(newPage);
+    setCurrentPage(newPage);
   };
 
   const handelSearchSubmit = (value) => {
     alert("A search was submitted: " + value);
   };
-  const handleClick = (value) => {
+  const handleTableClick = (value) => {
     alert("Click " + value);
   };
   // const handleButtonsetClick = (value) => {
@@ -58,12 +62,12 @@ const UsersPage = (props) => {
         <br />
         <PortalSearchPager
           currentPage={currentPage}
-          pages={25}
+          pages={10}
           pHolder={"חיפוש משתמשים"}
           onPageChange={handlePageChange}
           onSearchSubmit={handelSearchSubmit}
         />
-        <PortalTable data={data} headers={headers} handleClick={handleClick} keyName="userid" />
+        <PortalTable data={data} headers={headers} handleClick={handleTableClick} keyName="userid" />
         {/* <UsersButtonSetComp handleClick={handleButtonsetClick} btnNames=/> */}
       </Container>
     </div>
