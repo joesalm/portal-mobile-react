feb2b620e2ca34d5ecc9206ef1477759d4d5ab7b
nirch <nirh2@yahoo.com>
2020-09-30 15:37:37 +0300

    Updates

---
+++ precache-manifest.ab7bf47b6089daba61cefef8cb3a8016.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca0096decabcb515af730722043aae2",
+    "url": "/portal-mobile-react/index.html"
+  },
+  {
+    "revision": "5c180194c7217fc319e4",
+    "url": "/portal-mobile-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bdaf536e690c4c75dbc4",
+    "url": "/portal-mobile-react/static/css/main.976212d1.chunk.css"
+  },
+  {
+    "revision": "5c180194c7217fc319e4",
+    "url": "/portal-mobile-react/static/js/2.c3638fc0.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/portal-mobile-react/static/js/2.c3638fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdaf536e690c4c75dbc4",
+    "url": "/portal-mobile-react/static/js/main.3373428d.chunk.js"
+  },
+  {
+    "revision": "c4bf277143bb4f75d3f5",
+    "url": "/portal-mobile-react/static/js/runtime-main.465eab5c.js"
+  },
+  {
+    "revision": "d582391da9a68daf10a2ed2514c33826",
+    "url": "/portal-mobile-react/static/media/Assistant-Bold.d582391d.ttf"
+  },
+  {
+    "revision": "f2bbc6bae2ee3ce641adc1bb1a655371",
+    "url": "/portal-mobile-react/static/media/Assistant-ExtraBold.f2bbc6ba.ttf"
+  },
+  {
+    "revision": "5e4d348ae3eca48143c0274a3124a9c0",
+    "url": "/portal-mobile-react/static/media/Assistant-ExtraLight.5e4d348a.ttf"
+  },
+  {
+    "revision": "5415f395c1567a5c19efc1dc2892927a",
+    "url": "/portal-mobile-react/static/media/Assistant-Light.5415f395.ttf"
+  },
+  {
+    "revision": "e2b46dd69f54e57767ceef1d5fc8e688",
+    "url": "/portal-mobile-react/static/media/Assistant-Regular.e2b46dd6.ttf"
+  },
+  {
+    "revision": "d6759edb35ac7f29a029caa1192c010d",
+    "url": "/portal-mobile-react/static/media/Assistant-SemiBold.d6759edb.ttf"
+  },
+  {
+    "revision": "56cce1d51ac2cf91f7b4bab2d6bf9fea",
+    "url": "/portal-mobile-react/static/media/approvement_icon.56cce1d5.svg"
+  },
+  {
+    "revision": "45d1435153a647457fb4059704cd113e",
+    "url": "/portal-mobile-react/static/media/arrow_dim.45d14351.svg"
+  },
+  {
+    "revision": "f9cb2f0dbcc223bfcde136389f055e1b",
+    "url": "/portal-mobile-react/static/media/arrow_down.f9cb2f0d.svg"
+  },
+  {
+    "revision": "fed8aff6258230b0d7f2896df5ff4975",
+    "url": "/portal-mobile-react/static/media/arrowback.fed8aff6.svg"
+  },
+  {
+    "revision": "7511bf61ad960490c3c80213ea54debc",
+    "url": "/portal-mobile-react/static/media/courses_icon.7511bf61.svg"
+  },
+  {
+    "revision": "2d5a0811841909e0d6709ce23941d722",
+    "url": "/portal-mobile-react/static/media/disconnect_icon.2d5a0811.svg"
+  },
+  {
+    "revision": "eda3bf933a366fb5a406f286d68dc252",
+    "url": "/portal-mobile-react/static/media/logo.eda3bf93.png"
+  },
+  {
+    "revision": "65cde7b3f8fd3565761c2fe07d1fd36c",
+    "url": "/portal-mobile-react/static/media/noun_back_arrow.65cde7b3.svg"
+  },
+  {
+    "revision": "8d78794cee26a656bb45230d1154e2f0",
+    "url": "/portal-mobile-react/static/media/noun_copy.8d78794c.svg"
+  },
+  {
+    "revision": "457dc1c05f8318831eefc9885b26e0ff",
+    "url": "/portal-mobile-react/static/media/noun_save.457dc1c0.svg"
+  },
+  {
+    "revision": "1a7d3b8bfbe3e489d779740f745b0623",
+    "url": "/portal-mobile-react/static/media/profile_icon.1a7d3b8b.svg"
+  },
+  {
+    "revision": "e76b10d05996d27ab3c29b8b8a463eee",
+    "url": "/portal-mobile-react/static/media/report-hours.e76b10d0.svg"
+  },
+  {
+    "revision": "0ae2011f0854199812333bbbac57648e",
+    "url": "/portal-mobile-react/static/media/users_icon.0ae2011f.svg"
+  }
+]);
