ab1df99b390c7d898d23d5b75c9dbc137367fc51
eyalmagnus <eyalmagnus@gmail.com>
2020-10-01 12:04:57 +0300

    Component ready

--- src/components/PortalMultipleSelect/PortalMultipleSelect.js
+++ src/components/PortalMultipleSelect/PortalMultipleSelect.js
@@ -7,9 +7,8 @@ import { Container } from "react-bootstrap";
 const PortalMultipleSelect = (props) => {
     const { title, optionsList, callSelected } = props;
     const [showDrop, setShowDrop] = useState(false);
-    let newOptions = optionsList.slice();
     let checkOptions = false;
-    for (let i = 0; i < newOptions.length; i++) { if (!optionsList[i].select) { checkOptions = true } };
+    for (let i = 0; i < optionsList.length; i++) { if (!optionsList[i].select) { checkOptions = true } };
     let selectedViewC = [];
 
     const handlePlusClick = () => {
@@ -17,44 +16,92 @@ const PortalMultipleSelect = (props) => {
     }
 
     const handleOptionClick = (i) => {
+        let newOptions = optionsList.slice();
         setShowDrop(false);
         newOptions[i].select = true;
         callSelected(newOptions, newOptions[i], true);
     }
 
     const handleDelete = (i) => {
+        let newOptions = optionsList.slice();
         newOptions[i].select = false;
         callSelected(newOptions, newOptions[i], true);
     }
 
     let dropContent = [];
-    for (let i = 0; i < newOptions.length; i++) {
-        if (!newOptions[i].select) {
-            dropContent.push(<li key={i} onClick={() => handleOptionClick(i)}>{newOptions[i].option}</li>)
+    for (let i = 0; i < optionsList.length; i++) {
+        if (!optionsList[i].select) {
+            dropContent.push(<li key={i} onClick={() => handleOptionClick(i)}>{optionsList[i].option}</li>)
         }
     };
 
-    for (let i = 0; i < newOptions.length; i++) {
-        if (newOptions[i].select) {
-            selectedViewC.push(<div className="selItem" key={i} >{newOptions[i].option}{" "}<img className="x" src={x} onClick={() => handleDelete(i)} /></div>)
+
+    for (let i = 0; i < optionsList.length; i++) {
+
+        if (optionsList[i].select) {
+            selectedViewC.push(<div className="selItem" key={i} >{optionsList[i].option}{" "}<img className="x" src={x} onClick={() => handleDelete(i)} /></div>)
         }
     };
-    console.log(newOptions);
+    console.log(optionsList);
 
     return (
-        <Container>
-            <div className="p-multi-select">
-                <h6>{title}</h6>
-
-                <div className="flexShow">
-                    <img className="plus" src={plusSign} onClick={handlePlusClick} />
-                    {showDrop ? <ul className="dropDown">{dropContent}</ul> : null}
-                    {selectedViewC}
-                </div>
+        <div className="p-multi-select">
+            <h6>{title}</h6>
+            <div className="flexShow">
+                <img className="plus" src={plusSign} onClick={handlePlusClick} />
+                {showDrop ? <ul className="dropDown">{dropContent}</ul> : null}
+                {selectedViewC}
             </div>
-        </Container>
+        </div>
     );
 }
 
 
 export default PortalMultipleSelect;
+
+// put in parent:
+// const [optionsList, setOptionsList] = useState([{
+//     option: "גלידה",
+//     optionLabel: "1",
+//     select: false,
+//   }, {
+//     option: "פנקייק",
+//     optionLabel: "2",
+//     select: false,
+//   }, {
+//     option: "צ'יפס",
+//     optionLabel: "3",
+//     select: false,
+//   }, {
+//     option: "שוקולד",
+//     optionLabel: "4",
+//     select: false,
+//   }, {
+//     option: "ופל בלגי",
+//     optionLabel: "5",
+//     select: false,
+//   }, {
+//     option: "שניצל",
+//     optionLabel: "6",
+//     select: false,
+//   }, {
+//     option: "קטשופ",
+//     optionLabel: "7",
+//     select: false,
+//   }, {
+//     option: "תפוצ'יפס",
+//     optionLabel: "8",
+//     select: false,
+//   }, {
+//     option: "וזהו",
+//     optionLabel: "9",
+//     select: false,
+//   }
+//   ]);
+
+// const title = "ארוחת בקר";
+
+//   const handleSelectedChange = (selectedItems, currentItem, addOrErase) => {
+//     console.log(selectedItems, currentItem, addOrErase);
+//     setOptionsList(selectedItems)
+//   }
