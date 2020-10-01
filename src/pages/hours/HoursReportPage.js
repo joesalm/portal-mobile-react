1500653d527fb949467875be09e41aa247bd7e5f
Juliasavransky <63636407+Juliasavransky@users.noreply.github.com>
2020-10-01 10:50:01 +0300

    begining the HoursReportPage

--- src/pages/hours/HoursReportPage.js
+++ src/pages/hours/HoursReportPage.js
@@ -3,19 +3,110 @@ import './HoursReportPage.css'
 import PortalNavbar from '../../components/navbar/PortalNavbar';
 import ActiveUserContext from '../../shared/activeUserContext'
 import { Redirect } from 'react-router-dom'
+import IconButtonsSet from '../../components/iconButtonsSet/IconButtonsSet';
+import CallingCMonthPickerTest from '../../components/PortalMonthPicker/CallingMonthPickerTest';
 
 const HoursReportPage = (props) => {
-    const { handleLogout } = props;
+    const { handleLogout, onSave, onAdd } = props;
     const activeUser = useContext(ActiveUserContext);
     
     if (!activeUser) {
         return <Redirect to='/' />
     }
+    
 
     return (
         <div className="p-hours-report">
             <PortalNavbar handleLogout={handleLogout}/>
-            <h1>דיווח שעות</h1>
+            <CallingCMonthPickerTest/>
+            <div className="num"> הס"כ שעות :  200</div>
+            <table className="hoursTabel">
+                <tbody>
+                <tr >
+                    <th>תהריך</th>
+                    <th>פרויקט</th>
+                    <th>נושא הפעילות</th>
+                    <th>סה"כ שעות</th>
+                </tr>
+                <tr className="red">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="green">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="yellow">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="red">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="green">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="yellow">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="red">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="green">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="yellow">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="red">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="green">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <tr className="yellow">
+                    <td>12.2.20</td>
+                    <td>up progress</td>
+                    <td>שם פעילות כללית</td>
+                    <td>9</td>
+                </tr>
+                <div className="last"></div>
+                </tbody>
+            </table>
+            <IconButtonsSet 
+              onSave={onSave}
+              onAdd={onAdd}
+            />
         </div>
     );
 }
